e47fa58cd1309a9a8c162eaccc8133e1248ab53a
Wei Huang <huangwei19810803@gmail.com>
2023-05-26 21:14:20 -0600

    add fetch tweet error info

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(()=>{
+  // load all tweets
+loadTweets();
+
+// the animation of add tweet btn
+(function chevronDownloop() {
+  $('.add-tweet-btn').animate({top: '70px'}, {
+    duration: 1000,
+    complete: function() {
+      $('.add-tweet-btn').animate({top: '80px'}, {
+        duration: 1000,
+        complete: chevronDownloop});
+    }});
+})();
+
+// when scorll the container, the scroll up button shows, and the navigation bar gets dispear
+$(window).on("scroll", function() {
+  // console.log($(document.body).scrollTop());
+  if ($(window).scrollTop() == 0) {
+    $('.scroll-up-btn').hide();
+    $('nav').css({
+      "opacity": "1"
+    });
+  } else {
+    $('.scroll-up-btn').show();
+    $('nav').css({
+      "opacity": "0"
+    });
+      
+  }
+});
+
+// add event listener to that red scroll up button
+$(".scroll-up-btn").click(function() {
+  $("html, body").animate({
+    scrollTop: 0
+  }, "fast");
+  return false;
+});
+
+// add event listener to add tweet btn
+$('.add-tweet-btn').on('click', function() {
+  $('.new-tweet').slideDown(500, function() {
+    $('.tweet-text').focus();
+  });
+});
+
+});
+
 const createTweetElement = (data)=>{
   let eachTweet = $(`
     <article class="tweets-article">
@@ -35,17 +84,30 @@ const createTweetElement = (data)=>{
 };
 
 const renderTweets = (users)=>{
+
+  const $container = $('.tweets-container').empty();
+
   let eachTweet = '';
   users.forEach(tweet=>{
     eachTweet = createTweetElement(tweet);
-    $('.tweets-container').append(eachTweet);
+    $container.prepend(eachTweet);
   });
     
 };
 
 const loadTweets = ()=>{
-  $.get('/tweets/', function(data) {
+  const $tweetError = $('.tweet-error');
+
+  $.get('/tweets/')
+  .then(data=>{
     renderTweets(data);
+  })
+  .catch(error=>{
+    $tweetError.empty();
+    $tweetError.append("⚠ Sever Error, can not fetch tweets. ⚠ ");
+    $tweetError.slideDown(400, function() {
+            
+    });
   });
     
 };
@@ -55,41 +117,48 @@ const loadTweets = ()=>{
 $("#new-tweet-form").on("submit", function(event) {
   event.preventDefault();
 
+  const $form = $(this);
+  const $input = $form.find('.tweet-text');
+  const $counter = $form.find('.counter');
+  const $container = $form.closest('.container');
+  const $tweetError = $container.find('.tweet-error');
+  const $tweetsContainer = $container.find('.tweets-container');
+
     // if the counter value shows negative, show some error and can not submit the form
-  if ($('.counter').val() < 0) {
-    $('.tweet-error').empty();
-    $('.tweet-error').append("⚠ You enter too much text, please limit it. ⚠ ");
-    $('.tweet-error').slideDown(400, function() {
+  if ($counter.val() < 0) {
+    $tweetError.empty();
+    $tweetError.append("⚠ You enter too much text, please limit it. ⚠ ");
+    $tweetError.slideDown(400, function() {
             
     });
 
     return;
-  } else if ($('.counter').val() == 140) {// if user didn't enter anything, show some error and can not submit the form
-    $('.tweet-error').empty();
-    $('.tweet-error').append("⚠ You haven't text yet, please text something. ⚠ ");
-    $('.tweet-error').slideDown(400, function() {
+  } else if ($counter.val() == 140) {// if user didn't enter anything, show some error and can not submit the form
+    $tweetError.empty();
+    $tweetError.append("⚠ You haven't text yet, please text something. ⚠ ");
+    $tweetError.slideDown(400, function() {
             
     });
     return;
   }
 
   // when user enter is correct, reset the counter to 140
-  $('.counter').val(140);
+  $counter.val(140);
   // when user enter is correct, reset the container by removing the error message
-  $('.tweet-error').slideUp(200, function() {
-    $('.tweet-error').empty();
+  $tweetError.slideUp(200, function() {
+    $tweetError.empty();
   });
 
   // get the user input
   const str = $("#new-tweet-form").serialize();
   console.log(str);
 
-  // do ajax post submit
-  $.ajax({
-    type: 'POST',
-    url: '/tweets/',
-    data: str,
-    success: function(data) {
+  $.post('/tweets/', str)
+    .then((data)=>{
+      $input.val('');
+      $input.focus();
+      $counter.html(140);
+      
       const articleHtml = `
             <article class="tweets-article">
             <header class="tweets-header">
@@ -116,70 +185,50 @@ $("#new-tweet-form").on("submit", function(event) {
             </footer>
             </article>
         `;
-      $('.tweets-container').prepend(articleHtml);
-
-      $('.tweet-text').val('');
-      $('.tweet-text').focus();
-    }
-  });
-});
+      $tweetsContainer.prepend(articleHtml);
 
 
+    })
 
-$(()=>{
-    // load all tweets
-  loadTweets();
+  // do ajax post submit
+  // $.ajax({
+  //   type: 'POST',
+  //   url: '/tweets/',
+  //   data: str,
+  //   success: function(data) {
+  //     const articleHtml = `
+  //           <article class="tweets-article">
+  //           <header class="tweets-header">
+  //               <div class="tweets-header-left">
+  //                   <img src="${data.user.avatars}" class="tweets-header-avatar"/>
+  //                   <span class="tweets-header-name">${data.user.name}</span>
+  //               </div>
+  //               <div>
+  //                   <span class="tweets-header-handler">${data.user.handle}</span>
+  //               </div>
+  //           </header>
+  //           <div class="tweets-header-content">
+  //               <p>${data.content.text}</p>
+  //           </div>
+  //           <footer>
+  //               <span class="tweets-header-created">
+  //                   ${$.timeago(data.created_at)}
+  //               </span>
+  //               <span class="icon-span">
+  //                   <button><i class="fa-solid fa-flag"></i></button>
+  //                   <button><i class="fa-solid fa-retweet"></i></button>
+  //                   <button><i class="fa-solid fa-heart"></i></button>
+  //               </span>
+  //           </footer>
+  //           </article>
+  //       `;
+  //     $('.tweets-container').prepend(articleHtml);
 
-  // the animation of add tweet btn
-  (function chevronDownloop() {
-    $('.add-tweet-btn').animate({top: '70px'}, {
-      duration: 1000,
-      complete: function() {
-        $('.add-tweet-btn').animate({top: '80px'}, {
-          duration: 1000,
-          complete: chevronDownloop});
-      }});
-  })();
-
-  // when scorll the container, the scroll up button shows, and the navigation bar gets dispear
-  $(window).on("scroll", function() {
-    // console.log($(document.body).scrollTop());
-    if ($(window).scrollTop() == 0) {
-      $('.scroll-up-btn').hide();
-      $('nav').css({
-        "opacity": "1"
-      });
-    } else {
-      $('.scroll-up-btn').show();
-      $('nav').css({
-        "opacity": "0"
-      });
-        
-    }
-  });
-
-  // add event listener to that red scroll up button
-  $(".scroll-up-btn").click(function() {
-    $("html, body").animate({
-      scrollTop: 0
-    }, "fast");
-    return false;
-  });
-
-  // add event listener to add tweet btn
-  $('.add-tweet-btn').on('click', function() {
-    $('.new-tweet').slideDown(500, function() {
-      $('.tweet-text').focus();
-    });
-  });
-  
-  // // when hover one tweet, should have some shadow 
-  // $( "tweets-article" ).hover(
-  //   function() {
-  //     $( this ).addClass( "tweet-hover" );
-  //   }, function() {
-  //     $( this ).removeClass( "tweet-hover" );
+  //     $('.tweet-text').val('');
+  //     $('.tweet-text').focus();
   //   }
-  // );
-  
+  // });
 });
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,20 @@ let leftWord = 140;
 
 // counter user enter charater number
 $(".tweet-text").on('input', function(event){
-    const key = event.keyCode || event.charCode;
+
+    const $tweetText = $(this);
+    const $form = $tweetText.closest('form');
+    const $counter = $form.find('.counter');
 
     leftWord = 140 - event.currentTarget.value.length;
     
-    $('.counter').val(leftWord);
+    $counter.val(leftWord);
 
     // set the counter element color according the counter value
     if(leftWord<0){
-        $('.counter').css('color', 'red');
+        $counter.addClass('counter-red');
     }else{
-        $('.counter').css('color', '#555555');
+        $counter.removeClass('counter-red');
     }
 })
 
